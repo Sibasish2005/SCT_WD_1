5308ca07597d3f13ddb1e4bf337495da8a54fca1
sibasish005 <sibasishchakraborti@gmail.com>
2025-07-18 02:10:05 +0530

    Initial commit: Add all project files with images

---
+++ script.js
@@ -0,0 +1,48 @@
+<<<<<<< HEAD
+let  tablinks =  document.getElementsByClassName("tab-links")
+let  tabContents =  document.getElementsByClassName("tab-contents")
+function opentab(tabName){
+    for( tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for( tabContent of tabContents){
+        tabContent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab")
+}
+
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openMenu(){
+    sidemenu.style.right = "0";
+}
+
+function closeMenu(){
+    sidemenu.style.right = "-200px";
+=======
+let  tablinks =  document.getElementsByClassName("tab-links")
+let  tabContents =  document.getElementsByClassName("tab-contents")
+function opentab(tabName){
+    for( tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for( tabContent of tabContents){
+        tabContent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab")
+}
+
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openMenu(){
+    sidemenu.style.right = "0";
+}
+
+function closeMenu(){
+    sidemenu.style.right = "-200px";
+>>>>>>> 4652307 (Initial commit)
+}
